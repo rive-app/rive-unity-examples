4f36fa012b5aa39e34ea6afdde492f6b1f5e08ab
Zach Plata <plata.zach@gmail.com>
2024-01-31 16:39:29 -0600

    Update rive-unity package and update new API changes

--- getting-started/Assets/Rive/RiveEvents.cs
+++ getting-started/Assets/Rive/RiveEvents.cs
@@ -1,36 +1,34 @@
+using Rive;
 using UnityEngine;
 
 //! An example implementation to access Rive Events.
 
-namespace  Rive
+[RequireComponent(typeof(RiveScreen))]
+public class RiveEvents : MonoBehaviour
 {
-    [RequireComponent(typeof(RiveScreen))]
-    public class RiveEvents : MonoBehaviour
+    private RiveScreen m_riveScreen;
+    void Start()
     {
-        private RiveScreen m_riveScreen;
-        void Start()
-        {
-            m_riveScreen = GetComponent<RiveScreen>();
-            m_riveScreen.OnRiveEvent += RiveScreen_OnRiveEvent;
-        }
+        m_riveScreen = GetComponent<RiveScreen>();
+        m_riveScreen.OnRiveEvent += RiveScreen_OnRiveEvent;
+    }
 
-        private void RiveScreen_OnRiveEvent(ReportedEvent reportedEvent)
-        {
-            Debug.Log($"Event received, name: \"{reportedEvent.name}\", secondsDelay: {reportedEvent.secondsDelay}");
-            
-            // Access specific event properties
-            if (reportedEvent.name.StartsWith("rating"))
-            {
-                var rating = reportedEvent["rating"];
-                var message = reportedEvent["message"];
-                Debug.Log($"Rating: {rating}");
-                Debug.Log($"Message: {message}");
-            }
-        }
+    private void RiveScreen_OnRiveEvent(ReportedEvent reportedEvent)
+    {
+        Debug.Log($"Event received, name: \"{reportedEvent.Name}\", secondsDelay: {reportedEvent.SecondsDelay}");
 
-        private void OnDisable()
+        // Access specific event properties
+        if (reportedEvent.Name.StartsWith("rating"))
         {
-            m_riveScreen.OnRiveEvent -= RiveScreen_OnRiveEvent;
+            var rating = reportedEvent["rating"];
+            var message = reportedEvent["message"];
+            Debug.Log($"Rating: {rating}");
+            Debug.Log($"Message: {message}");
         }
+    }
+
+    private void OnDisable()
+    {
+        m_riveScreen.OnRiveEvent -= RiveScreen_OnRiveEvent;
     }
 }
--- getting-started/Assets/Rive/RiveTexture.cs
+++ getting-started/Assets/Rive/RiveTexture.cs
@@ -2,119 +2,118 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEditor;
+using Rive;
 
 using LoadAction = UnityEngine.Rendering.RenderBufferLoadAction;
 using StoreAction = UnityEngine.Rendering.RenderBufferStoreAction;
 
-namespace Rive
+[ExecuteInEditMode]
+public class RiveTexture : MonoBehaviour
 {
-    [ExecuteInEditMode]
-    public class RiveTexture : MonoBehaviour
+    public Rive.Asset asset;
+    public RenderTexture renderTexture;
+    public Fit fit = Fit.contain;
+    public Alignment alignment = Alignment.Center;
+
+    private Rive.RenderQueue m_renderQueue;
+    private Rive.Renderer m_riveRenderer;
+    private CommandBuffer m_commandBuffer;
+
+    private Rive.File m_file;
+    private Artboard m_artboard;
+    private StateMachine m_stateMachine;
+
+    private Camera m_camera;
+
+    private void Start()
     {
-        public Rive.Asset asset;
-        public RenderTexture renderTexture;
-        public Fit fit = Fit.contain;
-        public Alignment alignment = Alignment.center;
-
-        private RenderQueue m_renderQueue;
-        private CommandBuffer m_commandBuffer;
-
-        private Rive.File m_file;
-        private Artboard m_artboard;
-        private StateMachine m_stateMachine;
-
-        private Camera m_camera;
-
-        private void Start()
+        m_renderQueue = new Rive.RenderQueue(renderTexture);
+        m_riveRenderer = m_renderQueue.Renderer();
+        if (asset != null)
         {
-            m_renderQueue = new RenderQueue(renderTexture);
-            if (asset != null)
-            {
-                m_file = Rive.File.load(asset);
-                m_artboard = m_file.artboard(0);
-                m_stateMachine = m_artboard?.stateMachine();
-            }
-
-            if (m_artboard != null && renderTexture != null)
-            {
-                m_renderQueue.align(fit, alignment, m_artboard);
-                m_renderQueue.draw(m_artboard);
-
-                m_commandBuffer = new CommandBuffer();
-                m_renderQueue.toCommandBuffer();
-                m_commandBuffer.SetRenderTarget(renderTexture);
-                m_commandBuffer.ClearRenderTarget(true, true, UnityEngine.Color.clear, 0.0f);
-                m_renderQueue.addToCommandBuffer(m_commandBuffer);
-                m_camera = Camera.main;
-                if (m_camera != null)
-                {
-                    Camera.main.AddCommandBuffer(CameraEvent.AfterEverything, m_commandBuffer);
-                }
-            }
+            m_file = Rive.File.Load(asset);
+            m_artboard = m_file.Artboard(0);
+            m_stateMachine = m_artboard?.StateMachine();
         }
 
-        private void Update()
+        if (m_artboard != null && renderTexture != null)
         {
+            m_riveRenderer.Align(fit, alignment, m_artboard);
+            m_riveRenderer.Draw(m_artboard);
 
-            HitTesting();
-
-            if (m_stateMachine != null)
+            m_commandBuffer = m_riveRenderer.ToCommandBuffer();
+            m_commandBuffer.SetRenderTarget(renderTexture);
+            m_commandBuffer.ClearRenderTarget(true, true, UnityEngine.Color.clear, 0.0f);
+            m_riveRenderer.AddToCommandBuffer(m_commandBuffer);
+            m_camera = Camera.main;
+            if (m_camera != null)
             {
-                m_stateMachine.advance(Time.deltaTime);
+                Camera.main.AddCommandBuffer(CameraEvent.AfterEverything, m_commandBuffer);
             }
         }
+    }
 
-        bool m_wasMouseDown = false;
-        private Vector2 m_lastMousePosition;
+    private void Update()
+    {
 
-        void HitTesting()
+        HitTesting();
+
+        if (m_stateMachine != null)
         {
-            Camera camera = Camera.main;
-
-            if (camera == null || renderTexture == null || m_artboard == null) return;
-
-            if (!Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
-                return;
-
-            Renderer rend = hit.transform.GetComponent<Renderer>();
-            MeshCollider meshCollider = hit.collider as MeshCollider;
-
-            if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
-                return;
-
-            Vector2 pixelUV = hit.textureCoord;
-
-            pixelUV.x *= renderTexture.width;
-            pixelUV.y *= renderTexture.height;
-
-            Vector3 mousePos = camera.ScreenToViewportPoint(Input.mousePosition);
-            Vector2 mouseRiveScreenPos = new(mousePos.x * camera.pixelWidth, (1 - mousePos.y) * camera.pixelHeight);
-
-            if (m_lastMousePosition != mouseRiveScreenPos || transform.hasChanged)
-            {
-                Vector2 local = m_artboard.localCoordinate(pixelUV, new Rect(0, 0, renderTexture.width, renderTexture.height), fit, alignment);
-                m_stateMachine?.pointerMove(local);
-                m_lastMousePosition = mouseRiveScreenPos;
-            }
-            if (Input.GetMouseButtonDown(0))
-            {
-                Vector2 local = m_artboard.localCoordinate(pixelUV, new Rect(0, 0, renderTexture.width, renderTexture.height), fit, alignment);
-                m_stateMachine?.pointerDown(local);
-                m_wasMouseDown = true;
-            }
-            else if (m_wasMouseDown)
-            {
-                m_wasMouseDown = false; Vector2 local = m_artboard.localCoordinate(mouseRiveScreenPos, new Rect(0, 0, renderTexture.width, renderTexture.height), fit, alignment);
-                m_stateMachine?.pointerUp(local);
-            }
+            m_stateMachine.Advance(Time.deltaTime);
         }
+    }
 
-        private void OnDisable()
+    bool m_wasMouseDown = false;
+    private Vector2 m_lastMousePosition;
+
+    void HitTesting()
+    {
+        Camera camera = Camera.main;
+
+        if (camera == null || renderTexture == null || m_artboard == null) return;
+
+        if (!Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit))
+            return;
+
+        UnityEngine.Renderer rend = hit.transform.GetComponent<UnityEngine.Renderer>();
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+
+        if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+            return;
+
+        Vector2 pixelUV = hit.textureCoord;
+
+        pixelUV.x *= renderTexture.width;
+        pixelUV.y *= renderTexture.height;
+
+        Vector3 mousePos = camera.ScreenToViewportPoint(Input.mousePosition);
+        Vector2 mouseRiveScreenPos = new(mousePos.x * camera.pixelWidth, (1 - mousePos.y) * camera.pixelHeight);
+
+        if (m_lastMousePosition != mouseRiveScreenPos || transform.hasChanged)
         {
-            if (m_camera != null && m_commandBuffer != null)
-            {
-                m_camera.RemoveCommandBuffer(CameraEvent.AfterEverything, m_commandBuffer);
-            }
+            Vector2 local = m_artboard.LocalCoordinate(pixelUV, new Rect(0, 0, renderTexture.width, renderTexture.height), fit, alignment);
+            m_stateMachine?.PointerMove(local);
+            m_lastMousePosition = mouseRiveScreenPos;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 local = m_artboard.LocalCoordinate(pixelUV, new Rect(0, 0, renderTexture.width, renderTexture.height), fit, alignment);
+            m_stateMachine?.PointerDown(local);
+            m_wasMouseDown = true;
+        }
+        else if (m_wasMouseDown)
+        {
+            m_wasMouseDown = false; Vector2 local = m_artboard.LocalCoordinate(mouseRiveScreenPos, new Rect(0, 0, renderTexture.width, renderTexture.height), fit, alignment);
+            m_stateMachine?.PointerUp(local);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (m_camera != null && m_commandBuffer != null)
+        {
+            m_camera.RemoveCommandBuffer(CameraEvent.AfterEverything, m_commandBuffer);
         }
     }
 }
