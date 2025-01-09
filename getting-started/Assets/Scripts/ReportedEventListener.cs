fdaa80ba5f8ccde0e3e35d32d065b183580061df
Adam <67035612+damzobridge@users.noreply.github.com>
2025-01-09 09:43:44 -0800

    fix: update ReportedEventListener to use TryGetNumber

--- getting-started/Assets/Scripts/ReportedEventListener.cs
+++ getting-started/Assets/Scripts/ReportedEventListener.cs
@@ -20,38 +20,39 @@ public class ReportedEventListener : MonoBehaviour
         if (evt.Name.StartsWith("rating"))
         {
 
-            // For Typesafe access to properties, use the TryGet* methods
-            for (uint i = 0; i < evt.PropertyCount; i++)
+            // Access properties directly and cast them
+            if (evt.Properties.TryGetValue("rating", out object rating))
             {
-                ReportedEvent.Property property = evt.GetProperty(i);
-
-                if (property.Name == "rating" && property.TryGetString(out string ratingValue))
-                {
-                    Debug.Log($"Rating: {ratingValue}");
-                }
-                else if (property.Name == "message" && property.TryGetString(out string messageValue))
-                {
-                    Debug.Log($"Message: {messageValue}");
-
-                }
-
+                float ratingValue = (float)rating;
+                Debug.Log($"Rating: {ratingValue}");
             }
 
-            // OR access properties directly and cast them
+            if (evt.Properties.TryGetValue("message", out object message))
+            {
+                string messageValue = message as string;
+                Debug.Log($"Message: {messageValue}");
+            }
 
             /*
-                        if (evt.Properties.TryGetValue("rating", out object rating))
+                        // OR For Typesafe access to properties, use the TryGet* methods
+                        for (uint i = 0; i < evt.PropertyCount; i++)
                         {
-                            float ratingValue = (float)rating;
-                            Debug.Log($"Rating: {ratingValue}");
-                        }
+                            ReportedEvent.Property property = evt.GetProperty(i);
 
-                        if (evt.Properties.TryGetValue("message", out object message))
-                        {
-                            string messageValue = message as string;
-                            Debug.Log($"Message: {messageValue}");
+                            if (property.Name == "rating" && property.TryGetNumber(out float ratingValue))
+                            {
+                                Debug.Log($"Rating: {ratingValue}");
+                            }
+                            else if (property.Name == "message" && property.TryGetString(out string messageValue))
+                            {
+                                Debug.Log($"Message: {messageValue}");
+
+                            }
+
                         }
                         */
+
+
 
         }
     }
